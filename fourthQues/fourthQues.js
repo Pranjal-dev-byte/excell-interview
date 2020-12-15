89fd6772295834195560cb6406e39891bd3f6e38
pranjal <pranjalmisra2410@gmail.com>
2020-12-15 19:47:23 +0530

    Minor changes

--- fourthQues/fourthQues.js
+++ fourthQues/fourthQues.js
@@ -1,4 +1,5 @@
 var axios = require("axios")
+// Made an API request to Github api to get user data of my Github profile
 const apiRequest=()=>{
     axios.get('https://api.github.com/users/Pranjal-dev-byte')
     .then((response) => {
